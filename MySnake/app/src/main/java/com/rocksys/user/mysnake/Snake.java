092f5f2e65d9cea722ef615d232aa11c61db1302
Richard Cao <cao_xuelei@hotmail.com>
2015-11-04 16:01:20 -0700

    need to test BackgroundView

--- MySnake/app/src/main/java/com/rocksys/user/mysnake/Snake.java
+++ MySnake/app/src/main/java/com/rocksys/user/mysnake/Snake.java
@@ -1,16 +1,65 @@
+/*
+ * Copyright (C) 2007 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.rocksys.user.mysnake;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
 
+/**
+ * Snake: a simple game that everyone can enjoy.
+ *
+ * This is an implementation of the classic Game "Snake", in which you control a serpent roaming
+ * around the garden looking for apples. Be careful, though, because when you catch one, not only
+ * will you become longer, but you'll move faster. Running into yourself or the walls will end the
+ * game.
+ *
+ */
 public class Snake extends Activity {
 
+    /**
+     * Constants for desired direction of moving the snake
+     */
+    public static int MOVE_LEFT = 0;
+    public static int MOVE_UP = 1;
+    public static int MOVE_DOWN = 2;
+    public static int MOVE_RIGHT = 3;
+
+    private static String ICICLE_KEY = "snake-view";
+
+    private SnakeView mSnakeView;
+
+    /**
+     * Called when Activity is first created. Turns off the title bar, sets up the content views,
+     * and fires up the SnakeView.
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.snake_layout);
+
+        mSnakeView = (SnakeView) findViewById(R.id.snake);
     }
 
     @Override
